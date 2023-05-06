779da4f1f9952faa68321a39701c4e40966e661a
Seweryn Golba <sgoolb@gmail.com>
2023-05-06 19:20:15 +0200

    lottery

--- index.js
+++ index.js
@@ -2,11 +2,26 @@ const btn = document.querySelector("#button");
 const ctn = document.querySelector(".container");
 const box = document.querySelector(".box");
 
+const result = [];
+
 const generateNumber = () => {
+  if (result.length === 5) {
+    return;
+  }
+
+  const lotteryNumber = Math.floor(Math.random() * 45 + 1);
+
+  for (let i = 0; i < result.length; i++) {
+    if (lotteryNumber === result[i]) {
+      return generateNumber();
+    }
+  }
+
   const div = document.createElement("div");
   ctn.appendChild(div);
   div.classList.add("number");
-  div.textContent = Math.floor(Math.random() * 45 + 1);
+  div.textContent = lotteryNumber;
+  result.push(lotteryNumber);
 };
 
 btn.addEventListener("click", generateNumber);
